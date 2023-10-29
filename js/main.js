61bc592cad1fb4bd9d844148127f37392797a520
YoHaiYo <12si47bun@naver.com>
2023-10-29 13:20:34 +0900

    SWIPER 템플릿 추가

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// hamburger menu button 
+/// hamburger menu button 
  document.addEventListener("DOMContentLoaded", function() {
   const hamburger = document.querySelector(".hamburger");
   const navMenu = document.querySelector(".nav-menu");
@@ -14,4 +14,28 @@
      navMenu.classList.remove("active");
    })
   )
+});
+
+
+/// swiper 
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
 });
