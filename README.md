768382f729e106945daa91e5a3f197bb8260230d
YoHaiYo <12si47bun@naver.com>
2023-11-05 16:55:52 +0900

    ☆반응형 기본처리완료/디자인입히기 전

--- README.md
+++ README.md
@@ -3,5 +3,3 @@
 **반응형 전 기본템플릿**
 https://github.com/YoHaiYo/my-first-portfolio/tree/53aa8303f0c440437e7532dffc787aff8f664857
 
-**기술스택 아이콘 및 트렌드 사이트**
-https://stackshare.io/tools/top
--- js/main.js
+++ js/main.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
     $("body").addClass("dark-box");
     // 여기서 this는 ".modal-btn"
     // $(this).data('target') 자체가 .menu-1, .menu-2가 된다 !
-    $($(this).data('target')).addClass("showdiv")
+    $($(this).data('target')).addClass("showdiv")    
   })
   // 모달메뉴 닫기
   $(".close-btn").click(function() {  
